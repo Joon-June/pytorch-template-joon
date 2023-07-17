706732980d34502f4fa2f56d13fc993e339e75ff
Joon <evenn33111@gmail.com>
2023-07-18 01:16:54 +0900

    update

--- train.py
+++ train.py
@@ -2,48 +2,48 @@ import argparse
 
 import torch
 
-from config import load_config
-from data import get_train_dataloader, DataFetcher, get_eval_dataloader
-from model import Model
+from libs.config import load_config
+from libs.data import DataFetcher, get_eval_dataloader, get_train_dataloader
+from libs.model import Model
 from trainer import Trainer
 
 
 def parse_arguments():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("config_path", type=str)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_path", type=str)
 
-	args = parser.parse_args()
-	return args
+    args = parser.parse_args()
+    return args
 
 
 def train():
-	args = parse_arguments()
-	config = load_config(args.config_path)
+    args = parse_arguments()
+    config = load_config(args.config_path)
 
-	model = Model()
+    model = Model()
 
-	criterion = None
+    criterion = None
 
-	optimizer = None
+    optimizer = None
 
-	device = torch.device("gpu" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-	train_dataloader = DataFetcher(get_train_dataloader(config), device=device)
-	eval_dataloader = get_eval_dataloader(config)
+    train_dataloader = DataFetcher(get_train_dataloader(config), device=device)
+    get_eval_dataloader(config)
 
-	trainer = Trainer(
-		config=config,
-		model=model,
-		criterion=criterion,
-		optimizer=optimizer,
-		device=device,
-		train_dataloader=train_dataloader,
-		eval_dataloader=None,
-		writer=None,
-	)
+    trainer = Trainer(
+        config=config,
+        model=model,
+        criterion=criterion,
+        optimizer=optimizer,
+        device=device,
+        train_dataloader=train_dataloader,
+        eval_dataloader=None,
+        writer=None,
+    )
 
-	trainer.train()
+    trainer.train()
 
 
-if __name__ == '__main__':
-	train()
+if __name__ == "__main__":
+    train()
