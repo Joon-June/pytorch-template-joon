eae9caaa9fc70b9bad639d5bb96c16f8bc11ed8d
Joon <evenn33111@gmail.com>
2023-07-18 01:18:48 +0900

    update

--- utils/util.py
+++ utils/util.py
@@ -3,14 +3,12 @@ import random
 import numpy as np
 import torch
 
-from config import Config
 
-
-def set_seeds(config: Config):
-    torch.manual_seed(config.seed)
-    torch.cuda.manual_seed(config.seed)
-    torch.cuda.manual_seed_all(config.seed)  # if use multi-GPU
+def set_seeds(seed: int):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if use multi-GPU
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    np.random.seed(config.seed)
-    random.seed(config.seed)
+    np.random.seed(seed)
+    random.seed(seed)
